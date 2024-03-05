f8931469954208b0e7cb4987b6ea88f399f9ac63
findolor <anakisci@gmail.com>
2024-03-05 15:09:21 +0300

    chore(sdk): add admin wallet to config

--- sdk/config.ts
+++ sdk/config.ts
@@ -4,6 +4,7 @@ interface Config {
   network: Network
   rpcUrl: string
   networkPhrase: string
+  adminWallet: string
   deployerContractId: string
   splitterWasmHash: string
 }
@@ -16,16 +17,18 @@ const TESNET_CONFIG: Config = {
   network: "testnet",
   rpcUrl: "https://soroban-testnet.stellar.org:443",
   networkPhrase: "Test SDF Network ; September 2015",
+  adminWallet: "GBOAWTUJNSI5VKE3MDGY32LJF723OCQ42XYLNJWXDHCJKRZSFV3PKKMY",
   splitterWasmHash:
-    "b626d0e0d18e783346178ce8b841bbd8b0a49a5e003060f39838615986eac7e1",
+    "90d9640c94769eae5a984158f7bc1445cf18f5402e9bf05d583ca0ef2751586f",
   deployerContractId:
-    "CB2WMMSG66I366CO6335U56UOEG3OU57YKNQICKR4LRVURG4L56XPW6L",
+    "CCYIPLDBZ2ZGWLGWNLPT6AMOVB5JJILYMTY3BA3AZ5KUCX4UZNJVJIZZ",
 }
 
 const FUTURENET_CONFIG: Config = {
   network: "futurenet",
   rpcUrl: "https://rpc-futurenet.stellar.org",
   networkPhrase: "Test SDF Future Network ; October 2022",
+  adminWallet: "GBOAWTUJNSI5VKE3MDGY32LJF723OCQ42XYLNJWXDHCJKRZSFV3PKKMY",
   // TODO: Update this hash
   splitterWasmHash:
     "b626d0e0d18e783346178ce8b841bbd8b0a49a5e003060f39838615986eac7e1",
