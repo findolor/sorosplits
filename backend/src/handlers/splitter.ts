28e8c2782dc384edb75c9f2c2148b338ba24c54c
findolor <anakisci@gmail.com>
2024-03-06 17:51:32 +0300

    feat(backend): add create route for splitter contract

--- backend/src/handlers/splitter.ts
+++ backend/src/handlers/splitter.ts
@@ -1,9 +1,12 @@
 import bearer from "@elysiajs/bearer"
 import jwt from "@elysiajs/jwt"
-import { Elysia } from "elysia"
+import { Elysia, t } from "elysia"
 import { AuthenticationError } from "../errors"
+import { Contract, Keypair } from "@stellar/stellar-sdk"
+import { PrismaClient } from "@prisma/client"
 
 export default new Elysia({ prefix: "/splitter" })
+  .decorate("prisma", new PrismaClient())
   .use(bearer())
   .use(
     jwt({
@@ -11,14 +14,57 @@ export default new Elysia({ prefix: "/splitter" })
       secret: process.env.JWT_SECRET as string,
     })
   )
-  .derive(async ({ jwt, bearer }) => {
-    const user = await jwt.verify(bearer)
+  .derive(async ({ jwt, bearer, prisma }) => {
+    const payload = await jwt.verify(bearer)
+    if (!payload) {
+      throw new AuthenticationError("Unauthorized")
+    }
+    const user = await prisma.user.findUnique({
+      where: { publicKey: payload.publicKey as string },
+    })
     if (!user) {
       throw new AuthenticationError("Unauthorized")
     }
-    return { publicKey: user.id }
+    return { user }
   })
-  .get("/:contractId/interactions", async ({ params: { contractId }, publicKey }) => {
-    console.log(publicKey)
-    return { contractId }
+  .post(
+    "/create",
+    async ({ user, body: { contractId }, prisma }) => {
+      // Make sure the contractId is valid
+      new Contract(contractId)
+
+      const splitterContract = await prisma.splitterContract.create({
+        data: {
+          address: contractId,
+          ownerId: user.id,
+        },
+      })
+
+      await prisma.user.update({
+        where: { id: user.id },
+        data: {
+          splitterContracts: {
+            connect: [splitterContract],
+          },
+        },
+      })
+
+      return true
+    },
+    {
+      body: t.Object({
+        contractId: t.String(),
+      }),
+    }
+  )
+  .get("/my-contracts", async ({ prisma, user }) => {
+    const data = await prisma.user.findUnique({ where: { id: user.id }, select: {
+      splitterContracts: {
+        select: {
+          address: true,
+          transactions: true
+        }
+      }
+    } })
+    return data?.splitterContracts
   })
