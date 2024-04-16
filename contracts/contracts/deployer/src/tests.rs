495980f02e35c8a7d5654b7b8b434228537f1c59
findolor <anakisci@gmail.com>
2024-04-16 10:36:31 +0300

    fix(contracts): rewrite incorrect tests

--- contracts/contracts/deployer/src/tests.rs
+++ contracts/contracts/deployer/src/tests.rs
@@ -123,6 +123,112 @@ fn test_deploy_from_address() {
 }
 
 #[test]
+fn test_deploy_diversifier() {
+    let env = Env::default();
+    env.mock_all_auths();
+
+    let deployer_client = DeployerClient::new(&env, &env.register_contract(None, Deployer));
+    let splitter_wasm_hash = env.deployer().upload_contract_wasm(splitter_contract::WASM);
+    let diversifier_wasm_hash = env
+        .deployer()
+        .upload_contract_wasm(diversfier_contract::WASM);
+    let deployer = Address::generate(&env);
+    let salt = BytesN::from_array(&env, &[0; 32]);
+
+    let splitter_init_args: Vec<Val> = (
+        Bytes::from_slice(&env, "Diversifier Contract".as_bytes()),
+        ScVal::from_xdr_base64("AAAAEAAAAAEAAAACAAAAEQAAAAEAAAACAAAADwAAAAVzaGFyZQAAAAAAAAoAAAAAAAAAAAAAAAAAAAfQAAAADwAAAAtzaGFyZWhvbGRlcgAAAAASAAAAAAAAAADpj6fg3ucLOk6Bhu//xjLZukCRXxO5Pn3xfYlxA/sWMgAAABEAAAABAAAAAgAAAA8AAAAFc2hhcmUAAAAAAAAKAAAAAAAAAAAAAAAAAAAfQAAAAA8AAAALc2hhcmVob2xkZXIAAAAAEgAAAAAAAAAAfNqRvTyoFTMCJ3LrSV1WtzDaNILzjamsvTOwQ0SQuMw=", Limits {
+            depth: 100,
+            len: 1000
+        }).unwrap(),
+        ScVal::from_xdr_base64("AAAAAAAAAAA=", Limits {
+            depth: 100,
+            len: 1000
+        }).unwrap(),
+    ).into_val(&env);
+    let init_args: Vec<Val> = (
+        deployer.clone(),
+        splitter_wasm_hash.clone(),
+        salt.clone(),
+        splitter_init_args,
+    )
+        .into_val(&env);
+
+    let (contract_id, init_result) =
+        deployer_client.deploy_diversifier(&deployer, &diversifier_wasm_hash, &salt, &init_args);
+
+    assert!(init_result.is_void());
+
+    let expected_auth = AuthorizedInvocation {
+        // Top-level authorized function is `deploy` with all the arguments
+        function: AuthorizedFunction::Contract((
+            deployer_client.address,
+            Symbol::new(&env, "deploy_diversifier"),
+            (
+                deployer.clone(),
+                diversifier_wasm_hash.clone(),
+                salt,
+                init_args,
+            )
+                .into_val(&env),
+        )),
+        // From `deploy` function the 'create contract' host function has to be authorized
+        sub_invocations: vec![AuthorizedInvocation {
+            function: AuthorizedFunction::CreateContractHostFn(CreateContractArgs {
+                contract_id_preimage: ContractIdPreimage::Address(ContractIdPreimageFromAddress {
+                    address: deployer.clone().try_into().unwrap(),
+                    salt: Uint256([0; 32]),
+                }),
+                executable: xdr::ContractExecutable::Wasm(xdr::Hash(
+                    diversifier_wasm_hash.into_val(&env),
+                )),
+            }),
+            sub_invocations: vec![],
+        }],
+    };
+    assert_eq!(env.auths(), vec![(deployer.clone(), expected_auth)]);
+
+    // Invoke contract to check that it is initialized
+    let client = diversfier_contract::Client::new(&env, &contract_id);
+
+    let diversifier_config = client.get_diversifier_config();
+    assert_eq!(diversifier_config.admin, deployer);
+    let config = client.get_config();
+    assert_eq!(config.admin, contract_id);
+    assert_eq!(
+        config.name,
+        Bytes::from_slice(&env, "Diversifier Contract".as_bytes())
+    );
+    assert_eq!(config.updatable, false);
+
+    let shareholder1: Address = ScVal::from_xdr_base64(
+        "AAAAEgAAAAAAAAAA6Y+n4N7nCzpOgYbv/8Yy2bpAkV8TuT598X2JcQP7FjI=",
+        Limits {
+            depth: 100,
+            len: 1000,
+        },
+    )
+    .unwrap()
+    .into_val(&env);
+    let shareholder2: Address = ScVal::from_xdr_base64(
+        "AAAAEgAAAAAAAAAAfNqRvTyoFTMCJ3LrSV1WtzDaNILzjamsvTOwQ0SQuMw=",
+        Limits {
+            depth: 100,
+            len: 1000,
+        },
+    )
+    .unwrap()
+    .into_val(&env);
+
+    let shares = client.list_shares();
+    assert_eq!(shares.len(), 2);
+    assert_eq!(shares.get(0).unwrap().shareholder, shareholder1);
+    assert_eq!(shares.get(0).unwrap().share, 2000);
+    assert_eq!(shares.get(1).unwrap().shareholder, shareholder2);
+    assert_eq!(shares.get(1).unwrap().share, 8000);
+}
+
+#[test]
 fn test_network_deploy() {
     let env = Env::default();
     env.mock_all_auths();
@@ -221,7 +327,7 @@ fn test_network_deploy() {
     let deployed_contracts = deployer_client.deploy_network(&deployer, &wasm_hashes, &network_args);
 
     // FIRST SPLITTER
-    let client = splitter_contract::Client::new(&env, &deployed_contracts.get(0).unwrap());
+    let client = splitter_contract::Client::new(&env, &deployed_contracts.get(1).unwrap());
     let config = client.get_config();
     assert_eq!(
         config.name,
@@ -233,7 +339,7 @@ fn test_network_deploy() {
     assert_eq!(shares.get(1).unwrap().shareholder, shareholder2);
 
     // SECOND DIVERSIFIER
-    let client = diversfier_contract::Client::new(&env, &deployed_contracts.get(1).unwrap());
+    let client = diversfier_contract::Client::new(&env, &deployed_contracts.get(4).unwrap());
     let config = client.get_config();
     assert_eq!(
         config.name,
@@ -243,13 +349,13 @@ fn test_network_deploy() {
     assert_eq!(shares.len(), 3);
     assert_eq!(
         shares.get(0).unwrap().shareholder,
-        deployed_contracts.get(0).unwrap()
+        deployed_contracts.get(1).unwrap()
     );
     assert_eq!(shares.get(1).unwrap().shareholder, shareholder3);
     assert_eq!(shares.get(2).unwrap().shareholder, shareholder4);
 
     // THIRD SPLITTER
-    let client = splitter_contract::Client::new(&env, &deployed_contracts.get(2).unwrap());
+    let client = splitter_contract::Client::new(&env, &deployed_contracts.get(10).unwrap());
     let config = client.get_config();
     assert_eq!(
         config.name,
@@ -259,11 +365,11 @@ fn test_network_deploy() {
     assert_eq!(shares.len(), 4);
     assert_eq!(
         shares.get(0).unwrap().shareholder,
-        deployed_contracts.get(0).unwrap()
+        deployed_contracts.get(1).unwrap()
     );
     assert_eq!(
         shares.get(1).unwrap().shareholder,
-        deployed_contracts.get(1).unwrap()
+        deployed_contracts.get(4).unwrap()
     );
     assert_eq!(shares.get(2).unwrap().shareholder, shareholder1);
     assert_eq!(shares.get(3).unwrap().shareholder, shareholder3);
