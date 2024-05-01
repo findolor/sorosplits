059c76242d03c8602a809cbd9a5beba07802b2c4
findolor <anakisci@gmail.com>
2024-05-01 11:53:56 +0300

    feat(frontend): add desktop only page to index

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,10 +2,12 @@ import useApiService from "@/hooks/useApi"
 import "../styles/globals.css"
 
 import type { AppProps } from "next/app"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { Toaster } from "react-hot-toast"
 import useAppStore, { TokenListItem } from "../store"
 import { Tooltip } from "react-tooltip"
+import Text from "@/components/Text"
+import Image from "next/image"
 
 export default function App({ Component, pageProps }: AppProps) {
   const { tokenList, setTokenList } = useAppStore()
@@ -19,6 +21,46 @@ export default function App({ Component, pageProps }: AppProps) {
     }
     fetch()
   }, [])
+
+  const [isMobileOrTablet, setIsMobileOrTablet] = useState(false)
+  useEffect(() => {
+    function handleResize() {
+      setIsMobileOrTablet(window.innerWidth <= 1024)
+    }
+
+    // Add event listener
+    window.addEventListener("resize", handleResize)
+
+    // Call handler right away so state gets updated with initial window size
+    handleResize()
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
+
+  if (isMobileOrTablet) {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen gap-4 relative">
+        <div className="flex flex-col absolute top-[10em] gap-2 px-[5em]">
+          <Text
+            text="Sorosplits is not available on mobile devices."
+            size="15"
+            lineHeight="20"
+            centered
+          />
+          <Text
+            text="Please visit us on your desktop to use the app."
+            size="15"
+            lineHeight="20"
+            centered
+          />
+        </div>
+        <div className="absolute">
+          <Image src="/logo.svg" alt="Logo" width={205} height={10} />
+        </div>
+      </div>
+    )
+  }
 
   return (
     <div>
